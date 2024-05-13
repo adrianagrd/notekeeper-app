0c8ae48c85e0799ac13163267847dcaa9ce84615
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-05-13 20:19:10 +0200

    Add validation helper functions and tests for enum and non-empty validation

---
+++ src/helpers/validation/test/validateEnum.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest';
+import { validateEnum } from '../validateEnum';
+
+describe('validateEnum', () => {
+  describe('Given a valid status value', () => {
+    it('should returns true for valid status value', () => {
+      const validNote = {
+        status: 'completed'
+      };
+      expect(validateEnum(validNote)).toBe(true);
+    });
+  });
+
+  describe('Given an invalid status value', () => {
+    it('should returns false for invalid status value', () => {
+      const invalidNote = {
+        status: 'in progress'
+      };
+      expect(validateEnum(invalidNote)).toBe(false);
+    });
+  });
+});
--- src/helpers/validation/test/validateType.test.js
+++ src/helpers/validation/test/validateType.test.js
@@ -1,16 +1,30 @@
 import { describe, it, expect } from 'vitest';
-import { validateType } from '../validateType.js';
+import { validateType } from '../validateType';
 
 describe('validateType', () => {
-    it('return true for the correct types', () => {
-        const note = { title: 'Note 1', content: 'Content 1', important: true, status: 'active', dueDate: new Date() };
-        const expectedTypes = { title: 'string', content: 'string', important: 'boolean', status: 'string', dueDate: 'object' };
-        expect(validateType(note, expectedTypes)).toBe(true);
+  describe('Given type string', () => {
+    it('should return true for valid note object', () => {
+      const validNote = {
+        name: 'Meeting',
+        description: 'Discuss project updates',
+        important: true,
+        status: 'pending',
+        dueDate: '2024-05-20'
+      };
+      expect(validateType(validNote)).toBe(true);
     });
+  }); 
 
-    it('return false for the incorrect types', () => {
-        const note = { title: 'Note 1', content: 123, important: 'yes', status: 'active', dueDate: '2022-12-31' };
-        const expectedTypes = { title: 'string', content: 'string', important: 'boolean', status: 'string', dueDate: 'object' };
-        expect(validateType(note, expectedTypes)).toBe(false);
+  describe('Given type number', () => {
+    it('should return false for invalid note object', () => {
+      const invalidNote = {
+        name: 'Meeting',
+        description: 'Discuss project updates',
+        important: true,
+        status: 'pending',
+        dueDate: 12345 
+      };
+      expect(validateType(invalidNote)).toBe(false);
     });
+  });
 });
