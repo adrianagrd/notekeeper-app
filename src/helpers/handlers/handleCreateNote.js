ace054c9e9edea7f6b6616d935e8befe76d11581
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-06-06 15:24:31 +0200

    Refactor handleCreateNote to reset form after note creation

--- src/helpers/handlers/handleCreateNote.js
+++ src/helpers/handlers/handleCreateNote.js
@@ -15,5 +15,6 @@ export const handleCreateNote = (event, handleUpdateNote) => {
         .then(() => notesService.getAllNotes())
         .then(({ notes }) => {
             handleUpdateNote(notes);
+            event.target.reset();
         });
 };
